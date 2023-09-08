59d1e134effdf78ee558b9b4ef648a66c2e0d885
Dax <dixitvora@gmail.com>
2023-09-08 10:51:29 +0530

    main: added basic structure of the app with folder

---
+++ start.sh
@@ -0,0 +1,9 @@
+cd apps/api; yarn init -y
+cd ../admin; yarn init -y
+cd ../client; yarn init -y
+cd ../doc; yarn init -y
+
+cd ../../packages/ui; yarn init -y
+cd ../logger; yarn init -y
+cd ../eslint-config-custom; yarn init -y
+cd ../eslint-config-server; yarn init -y
